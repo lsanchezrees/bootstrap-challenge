a7aac09335db3a48bcab4abe273e04e15990a641
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-02 21:08:17 -0600

    added jumbotron and styling for html and jumbotron

--- index.php
+++ index.php
@@ -33,6 +33,15 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
+		<div class="jumbotron">
+			<h1 class="display-3">Hello, world!</h1>
+			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+			<hr class="my-4">
+
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			</p>
+		</div>
 
 
 
