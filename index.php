8fec21ea4c9b61ac1da8f74d1dcfb39529593a81
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-04 17:42:04 -0600

    added images and text, rows and columns to main container

--- index.php
+++ index.php
@@ -42,6 +42,50 @@
 				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 			</p>
 		</div>
+		<div class="container">
+			<div class="row"><!--first row-->
+				<div class="col-md-3">
+					<img src="http://loremflickr.com/220/220/dog" class="img-responsive" alt="dog" />
+				</div><!--column-->
+				<div class="col-md-9">
+					<p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.</p>
+
+					<p>Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.</p>
+				</div><!--column-->
+			</div><!--row-->
+
+			<div class="row"><!--second row-->
+				<div class="col-md-3">
+					<img src="http://loremflickr.com/220/220/flower" class="img-responsive img-circle" alt="flower"/>
+				</div>
+				<div class="col-md-3">
+					<img src="http://loremflickr.com/220/220/duck" class="img-responsive img-circle" alt="duck"/>
+				</div>
+				<div class="col-md-3">
+					<img src="http://loremflickr.com/220/220/rain" class="img-responsive img-circle" alt="rain"/>
+				</div>
+				<div class="col-md-3">
+					<img src="http://loremflickr.com/220/220/puppy" class="img-responsive img-circle" alt="puppy"/>
+				</div><!--column-->
+			</div><!--row-->
+
+			<div class="row"><!--third row-->
+				<div class="col-md-3">
+					<p>Skysail nipper hogshead cutlass Sea Legs fluke me stern Cat o'nine tails poop deck. Clap of thunder topgallant jury mast belay holystone clipper poop deck no prey, no pay spike aft. Nipperkin take a caulk jury mast Sail ho black jack bilge water black spot interloper draft aft.</p>
+
+				</div>
+				<div class="col-md-3">
+					<p>Schooner come about pressgang pinnace chase port parley dance the hempen jig knave nipper. Boatswain nipperkin American Main to go on account lanyard spirits case shot belay coxswain Sail ho. Run a shot across the bow Brethren of the Coast smartly yawl barque grapple rum parley chase guns come about.</p>
+				</div>
+				<div class="col-md-3">
+					<p>Squiffy lee cutlass Letter of Marque chase guns cable rutters schooner line smartly. Swing the lead nipperkin fluke pink shrouds matey Privateer quarter Nelsons folly hogshead. Chantey gabion parley dead men tell no tales pressgang jolly boat splice the main brace tack fire ship Jack Ketch.</p>
+				</div>
+				<div class="col-md-3">
+					<p>ucko maroon belaying pin fore lookout list Sink me aft transom belay. Broadside yardarm Sea Legs avast fore gangway yawl spanker quarter squiffy. Pinnace hempen halter clap of thunder stern lanyard wench smartly ho Spanish Main dead men tell no tales.</p>
+				</div><!--column-->
+			</div><!--row-->
+
+		</div><!--container-->
 
 
 
